0e6399da8f0d3071bee428522e9289358aea02c5
joaquin1990 <joaquingarese@gmail.com>
2023-05-24 09:05:05 -0300

    query of sanity

--- src/queries/farms.ts
+++ src/queries/farms.ts
@@ -1,15 +1,28 @@
+// import { groq } from 'next-sanity';
+
+// export default groq`{
+//     "farms": *[_type == "farms"] {
+//         ...,
+//         mainImage {
+//             ...,
+//             image {
+//             "width": asset->metadata.dimensions.width,
+//             "height": asset->metadata.dimensions.height,
+//             asset->
+//             }
+//         }
+//     }
+// }`;
+
 import { groq } from 'next-sanity';
 
 export default groq`{
     "farms": *[_type == "farms"] {
         ...,
-        mainImage {
-            ...,
-            image {
+            imagen {
             "width": asset->metadata.dimensions.width,
             "height": asset->metadata.dimensions.height,
             asset->
             }
-        }
     }
 }`;
