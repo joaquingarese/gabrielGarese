83414fc9a3a051eaa04d52fe92b774773fe74f2a
joaquin1990 <joaquingarese@gmail.com>
2023-06-11 15:52:30 -0300

    ok

--- src/components/houses/Houses.tsx
+++ src/components/houses/Houses.tsx
@@ -4,16 +4,15 @@ import HouseContainer from './HouseContainer';
 import { House2 } from '~/pages/types';
 
 interface PropertiesProps {
-  houses: Array<House2>;
+  houses: House2[];
 }
 
-// function Properties({ houses }) {
 const Houses = ({ houses }: PropertiesProps) => {
   return (
     <>
       <div className="container mt-32 md:mt-10 flex-column">
         <h2 className=" text-2xl md:text-3xl font-title mt-4 font-medium">PROPIEDADES</h2>
-        <p className="font-navbar mt-4  xs:w-3/4  lg:w-1/3 text-textNotes font-thin">
+        <p className="font-navbar mt-4 xs:w-3/4 lg:w-1/3 text-textNotes font-thin">
           Seleccione localidad:
         </p>
         <HouseContainer properties={houses} />
