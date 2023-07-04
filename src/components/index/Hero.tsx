8529dfaf18e656faed2d0f8c146534c3b2fb651b
joaquin1990 <joaquingarese@gmail.com>
2023-07-04 11:30:31 -0300

    HeroSlider Commented everywhere

--- src/components/index/Hero.tsx
+++ src/components/index/Hero.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
-import HeroSlider from '../UIElements/HeroSlider';
+// import HeroSlider from '../UIElements/HeroSlider';
 import { useState, useEffect } from 'react';
 
 function Hero() {
