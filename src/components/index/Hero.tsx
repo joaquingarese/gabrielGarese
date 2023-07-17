53d53cab9a637259fdcb023cfd968067aba05ebd
joaquin1990 <joaquingarese@gmail.com>
2023-07-17 18:54:58 -0300

    to see if the loader is fixed

--- src/components/index/Hero.tsx
+++ src/components/index/Hero.tsx
@@ -23,7 +23,7 @@ function Hero() {
   }, []);
 
   return loading ? (
-    <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-10">
+    <div className="fixed h-full inset-0 flex items-center justify-center z-50 bg-black bg-opacity-10">
       <BeatLoader size={50} />
     </div>
   ) : (
