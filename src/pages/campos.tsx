5ba7353ea382d95e05a53f8ece5d0a6a969b1a18
joaquin1990 <joaquingarese@gmail.com>
2023-07-19 09:41:27 -0300

    Removing featured Farms

--- src/pages/campos.tsx
+++ src/pages/campos.tsx
@@ -1,28 +1,30 @@
 import Farms from '~/components/farms/Farms';
-import { getClient } from '~/lib/sanity.server';
-import farmsQuery from '~/queries/farms';
+// import { getClient } from '~/lib/sanity.server';
+// import farmsQuery from '~/queries/farms';
 
-interface CamposProps {
-  farms: Farm2[];
-}
+// interface CamposProps {
+//   farms: Farm2[];
+// }
 
-function Campos({ farms }: CamposProps) {
+// function Campos({ farms }: CamposProps) {
+function Campos() {
   return (
     <>
-      <Farms farms={farms} />
+      {/* <Farms farms={farms} /> */}
+      <Farms />
     </>
   );
 }
 
-export async function getStaticProps() {
-  const farms = await getClient().fetch(farmsQuery);
+// export async function getStaticProps() {
+//   const farms = await getClient().fetch(farmsQuery);
 
-  return {
-    props: {
-      farms: farms.farms || []
-    },
-    revalidate: 60
-  };
-}
+//   return {
+//     props: {
+//       farms: farms.farms || []
+//     },
+//     revalidate: 60
+//   };
+// }
 
 export default Campos;
