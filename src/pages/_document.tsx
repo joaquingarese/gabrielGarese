bf812ae2226df6047085bab458873526d30aceb9
joaquin1990 <joaquingarese@gmail.com>
2023-08-07 17:18:45 -0300

    _document modifications

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,38 +7,29 @@ export default function Document() {
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta property="og:type" content="website" />
-        {/* Cambiar descripcion */}
-        <meta name="description" content="Tresbe - Importamos lo que necesitás" />
-        {/*Preguntarle a pipe pagina que te ayuda a ponerle el favicon */}
-        {/* <link rel="shortcut icon" href="/favicon.png" type="image/png" /> */}
-        {/* Pipe me ayuda con esta  */}
-        {/* <meta name="msapplication-TileImage" content="/public/favicon.png" /> */}
 
-        {/* Poner la pagina de papa*/}
-        <link rel="canonical" href="https://www.tresbe.com.uy" />
+        <meta name="description" content="Tresbe - Importamos lo que necesitás" />
+
+        <link rel="canonical" href="https://www.gabrielgarese.com" />
         <meta name="robots" content="index, follow" />
 
-        {/* conetent gabriel garese en las dos sigueintes */}
-        <meta name="apple-mobile-web-app-title" content="Tresbe" />
-        <meta name="application-name" content="Tresbe" />
-        {/* cambiar color para relacionado con la pagina */}
-        <meta name="msapplication-TileColor" content="#0098DA" />
+        <meta name="apple-mobile-web-app-title" content="Gabriel Garese" />
+        <meta name="application-name" content="Gabriel Garese" />
+
+        <meta name="msapplication-TileColor" content="#8D7449" />
         <meta name="twitter:card" content="summary_large_image" />
-        {/* Pipe me ayuda */}
-        {/* <meta property="og:image" content={'https://www.tresbe.com.uy/favicon.png'} /> */}
-        {/* relacionado a pagina de papa en las dos */}
+
         <meta property="og:description" content="Gabriel Garese - Negocios Rurales" />
-        <meta property="og:title" content="Tresbe" />
-        {/* Pagina de papa */}
-        <meta property="og:url" content="https://www.tresbe.com.uy/" />
+        <meta property="og:title" content="Gabriel Garese - Negocios Rurales" />
+
+        <meta property="og:url" content="https://www.gabrielgarese.com" />
+        <meta
+          name="keywords"
+          content="Gabriel Garese, campos, comprar campos, negocios rurales, hectáreas"
+        />
+        <title>Gabriel Garese - Negocios Rurales</title>
 
         <meta name="twitter:card" content="summary_large_image" />
-        {/* <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#FBFBF8" />
-        <meta name="msapplication-TileColor" content="#0098DA" />
-        <meta name="theme-color" content="#ffffff" /> */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
--- src/pages/nosotros.tsx
+++ src/pages/nosotros.tsx
@@ -1,8 +1,16 @@
+// import Head from 'next/head';
+
 import Company from '~/components/company/Company';
 
 const Empresa = () => {
   return (
     <>
+      {/* <Head>
+        <meta
+          name="keywords"
+          content="Gabriel Garese, campos, comprar campos, negocios rurales, hectáreas"
+        />
+      </Head> */}
       {/* <Container> */}
       <Company />
       {/* </Container> */}
